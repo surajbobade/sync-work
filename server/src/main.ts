24a1a5c2027d5427dc0edb7a66d1e343d16a1f4b
Suraj Bobade <suraj@192.168.1.6>
2026-06-08 23:39:20 +0530

    fix: cors restrictions

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,7 +16,10 @@ async function bootstrap() {
     app.use(cookieParser());
 
     app.enableCors({
-        origin: 'http://localhost:5173',
+        origin: [
+            'http://localhost:5173',
+            'https://sync-work-two.vercel.app',
+        ],
         credentials: true,
     });
 
